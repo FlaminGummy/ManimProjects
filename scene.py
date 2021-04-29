366303cba819581c0c187a1776cfee1686187909
saieeshb <saieeshpulimatibairam@gmail.com>
2021-04-29 20:43:10 +0530

    graph

--- scene.py
+++ scene.py
@@ -34,11 +34,11 @@ class TestG(GraphScene):
     def __init__(self, **kwargs):
       GraphScene.__init__(
           self,
-          x_min = -5 * PI,
+          x_min = 0,
           x_max= 5 * PI,
-          y_min= -3,
+          y_min= 0,
           y_max= 3,
-          graph_origin= ORIGIN,
+          graph_origin= LEFT,
           **kwargs
       )
     def construct(self):
